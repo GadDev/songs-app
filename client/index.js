4575e79c77f8c171c7631620622b8a8654b58eb3
algadaix <alexandre@qured.com>
2021-02-20 20:15:01 +0000

    MongoDB Atlas Setup and Configuration

---
+++ client/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Root = () => {
+	return <div>Songs</div>;
+};
+
+ReactDOM.render(<Root />, document.querySelector('#root'));
--- server/server.js
+++ server/server.js
@@ -4,19 +4,28 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
-
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 
+const PASSWORD = process.env.PASSWORD;
+const USER = process.env.USERDB;
+console.log(PASSWORD);
+console.log(USER);
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@cluster0.9rt9n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log('=========================>', MONGO_URI);
 if (!MONGO_URI) {
 	throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 mongoose.connection
-	.once('open', () => console.log('Connected to MongoLab instance.'))
+	.once('open', () => console.log('Connected to Mongo Atlas instance.'))
 	.on('error', (error) =>
 		console.log('Error connecting to MongoLab:', error)
 	);
